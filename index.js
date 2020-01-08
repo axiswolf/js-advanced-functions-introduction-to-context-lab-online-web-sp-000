c554ba2db453275e65275a04d319c3560f7d35ee
Karen V <karenvo@usc.edu>
2020-01-08 11:09:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  return 
+  "firstName": firstName,
+  "familyName": familyName,
+  "title": title,
+  "payPerHour": payPerHour,
+  "timeInEvents": [],
+  "timeOutEvents": []
+}
