e02ac7b8d6a982ad4057cb5d7a70c630fe561531
Karen V <karenvo@usc.edu>
2020-01-08 11:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function wagesEarnedOnDate(record, date) {
 function findEmployeeByFirstName(employeeRecords, name) {
   // finds "Loki"
   return employeeRecords.find(x => employeeRecords.name == name);
-  
+
 }
 
 function calculatePayroll() {
